dcead0fd5344ff36c0a9e33611a65cd46603d313
Václav Pilát <pilatvacl@seznam.cz>
2021-08-02 21:09:39 +0200

    Added Destination and Pattern variables

--- Xonotic-music.sh
+++ Xonotic-music.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
 
-DLCACHE=~/.xonotic/data/dlcache
+# Xonotic dlcache location
+DLCACHE=~/.xonotic/data/dlcache/
+
+# Getting destination folder from arguments (If not set, a default value is used instead)
+[ ! -z "$1" ] && DESTINATION=$1 || DESTINATION=/tmp/xonotic-music/
+
+# Getting pattern from arguments (If pattern is not set, a default pattern is used instead)
+[ ! -z "$2" ] && PATTERN=$2 || PATTERN=radio-*
 
 # Checking if there is a dlcache
 if [ -e "$DLCACHE" ]
 then
-    echo "Xonotic dlcache found at \"$DLCACHE\", contains X files"
     cd "$DLCACHE"
+    shopt -s nullglob # Empty directory returns an empty array
+    FILES=($PATTERN)
+    echo "Xonotic dlcache found at \"$DLCACHE\"."
+    echo "Directory contains `ls | wc -l` files (${#FILES[@]} of them match pattern \"$PATTERN\")."
+    echo "Music files will be copied into \"$DESTINATION\"."
 else
-    echo "Xonotic dlcache was not found: \"$DLCACHE\""
+    echo "Xonotic dlcache was not found: \"$DLCACHE\" (doesn't exist or isn't a directory)"
 fi
