fe03ec945b6529afc5c20117291012e2942a93b3
Václav Pilát <pilatvacl@seznam.cz>
2021-08-03 11:20:53 +0200

    Removing unnecessary files

--- Xonotic-music.sh
+++ Xonotic-music.sh
@@ -21,15 +21,25 @@ then
     BEFORE=`ls $DESTINATION | wc -l` # Number of files before unzipping
     echo "Music files will be copied into \"$DESTINATION\". This folder already contains $BEFORE files."
     INDEX=0
-    for FILE in "${FILES[@]}"
+    for FILE in "${FILES[@]}" # Extracting files from each archive
     do
         INDEX=$((INDEX + 1))
-        #unzip -nq $FILE -d $DESTINATION -x "INFO" "LICENSE" 2>&- # Supressing errors while unzipping files
-        unzip -nq $FILE -p "*.ogg" -d $DESTINATION 2>&- # Supressing errors while unzipping files
-        echo "$INDEX / ${#FILES[@]} :: $FILE"
+        NOW=`ls $DESTINATION | wc -l` # Number of files right now
+        unzip -nq $FILE -d $DESTINATION -x "INFO" "LICENSE" 2>&- # Supressing errors while unzipping files
+        if [ $? == 0 ] # Looking for errors
+        then
+            MESSAGE="OK"
+        else
+            MESSAGE="ERROR"
+        fi
+        echo "$INDEX / ${#FILES[@]} :: $FILE :: $MESSAGE ($((`ls $DESTINATION | wc -l` - $NOW)) files extracted)"
     done
     AFTER=`ls $DESTINATION | wc -l` # Number of files after unzipping
-    echo "Unzipping complete. $(($AFTER - $BEFORE)) files out of ${#FILES[@]} were unzipped successfully. Folder now contains $AFTER files."
+    echo "Unzipping complete. $(($AFTER - $BEFORE)) files out of ${#FILES[@]} archives were unzipped successfully. Folder now contains $AFTER files."
+    cd $DESTINATION
+    find . ! -name "*.ogg" ! -name "." ! -name ".." -exec rm -rf {} \; 2>&- # Supressing errors while removing unnecessary files
+    NOW=`ls $DESTINATION | wc -l` # Number of files right now
+    echo "Cleaning complete. Removed $(($AFTER - $NOW)) unnecessary files (out of $AFTER files). Folder now contains $NOW files."
 else
     echo "Xonotic dlcache was not found: \"$DLCACHE\" (doesn't exist or isn't a directory)"
 fi
